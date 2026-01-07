f8d5cb392bbe433955f95165123da70e80c95d52
stsvt <taras.stasiv3@gmail.com>
2026-01-08 01:06:44 +0200

    feat: install prettier and tailwind.css

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import checker from 'vite-plugin-checker';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    tailwindcss(),
+    checker({ eslint: { lintCommand: 'eslint "./src/**/*.{ts,tsx}"' } }),
+  ],
+});
