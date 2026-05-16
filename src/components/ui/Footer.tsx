f50b09e10a5648d048f7eef437f9009effa2282e
hvlvk <vikagevel19@gmail.com>
2026-05-16 16:18:23 +0300

    refactor: clean up Footer and Header components by removing unused code and optimizing styles

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -1,9 +1,3 @@
-import {
-  GithubLogoIcon,
-  TelegramLogoIcon,
-  LinkedinLogoIcon,
-  InstagramLogoIcon,
-} from '@phosphor-icons/react';
 import Logo from './Logo.tsx';
 import '../../styles/footer.css';
 
@@ -21,91 +15,6 @@ export default function Footer() {
             Ваш улюблений сервіс для відслідковування та оцінювання фільмів,
             серіалів та мультфільмів.
           </p>
-          <div className='footer-social'>
-            <a
-              href='https://github.com'
-              target='_blank'
-              rel='noopener noreferrer'
-              aria-label='GitHub'
-            >
-              <GithubLogoIcon size={24} />
-            </a>
-            <a
-              href='https://instagram.com'
-              target='_blank'
-              rel='noopener noreferrer'
-              aria-label='Instagram'
-            >
-              <InstagramLogoIcon size={24} />
-            </a>
-            <a
-              href='https://t.me'
-              target='_blank'
-              rel='noopener noreferrer'
-              aria-label='Telegram'
-            >
-              <TelegramLogoIcon size={24} />
-            </a>
-            <a
-              href='https://linkedin.com'
-              target='_blank'
-              rel='noopener noreferrer'
-              aria-label='LinkedIn'
-            >
-              <LinkedinLogoIcon size={24} />
-            </a>
-          </div>
-        </div>
-
-        <div className='footer-section'>
-          <h4 className='footer-title'>Навігація</h4>
-          <ul className='footer-links'>
-            <li>
-              <a href='/'>Головна</a>
-            </li>
-            <li>
-              <a href='/#upcoming'>Фільми</a>
-            </li>
-            <li>
-              <a href='/#nowPlaying'>Серіали</a>
-            </li>
-            <li>
-              <a href='/#topRated'>Мультфільми</a>
-            </li>
-          </ul>
-        </div>
-
-        <div className='footer-section'>
-          <h4 className='footer-title'>Допомога</h4>
-          <ul className='footer-links'>
-            <li>
-              <a href='#'>Часті питання</a>
-            </li>
-            <li>
-              <a href='#'>Контакти</a>
-            </li>
-            <li>
-              <a href='#'>Звіти про помилки</a>
-            </li>
-            <li>
-              <a href='#'>Запити функцій</a>
-            </li>
-          </ul>
-        </div>
-
-        <div className='footer-section'>
-          <h4 className='footer-title'>Правила</h4>
-          <ul className='footer-links'>
-            <li>
-              <a href='#'>Політика конфіденційності</a>
-            </li>
-            <li>
-              <a href='#'>Умови використання</a>
-            </li>
-            <li>
-              <a href='#'>Налаштування cookies</a>
-            </li>
-          </ul>
         </div>
       </div>
 
